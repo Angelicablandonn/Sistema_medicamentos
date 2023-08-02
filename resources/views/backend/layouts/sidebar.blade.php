69f168a9723e7071119b5f0b8c1ffdcd462d3855
Angelicablandonn <angeliblan99@gmail.com>
2023-08-02 14:20:35 -0500

    sidebar

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -21,103 +21,115 @@
             </div>
         </div>
     </div>
+    <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <!-- ... Código del panel de usuario ... -->
+        </div>
 
-    <!-- Sidebar Menu -->
-    <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-             with font-awesome or any other icon font library -->
+        <!-- SidebarSearch Form -->
+        <div class="form-inline">
+            <!-- ... Código del formulario de búsqueda ... -->
+        </div>
 
-            <li class="nav-item">
-                <a href="{{ route('ventas.index') }}" class="nav-link">
-                    <i class="nav-icon fas fa-th"></i>
-                    <p>
-                        Facturacion
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
+                <!-- Facturación -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-file-invoice"></i>
+                        <p>Facturación</p>
+                        <span class="right badge badge-danger">Nuevo</span>
+                    </a>
+                </li>
 
-                    </p>  <span class="right badge badge-danger">Nuevo</span>
-                </a>
-            </li>
+                <!-- Ventas -->
+                <li class="nav-item">
+                    <a href="{{ route('ventas.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-shopping-cart"></i>
+                        <p>Ventas</p>
+                    </a>
+                </li>
 
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-edit"></i>
-                    <p>
-                        Usuarios
-                        <i class="fas fa-angle-left right"></i>
-                    </p>
-                </a>
-                <ul class="nav nav-treeview">
-                    <li class="nav-item">
-                        <a href="{{ route('users.index') }}" class="nav-link">
+                <!-- Usuarios -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>
+                            Usuarios
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('users.index') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Listar Usuarios</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('users.create') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Agregar Usuarios</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- Medicamentos -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-medkit"></i>
+                        <p>
+                            Medicamentos
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('medicamento.index') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Listar Medicamentos</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('medicamento.create') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Agregar Medicamentos</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- Pedidos -->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-clipboard-list"></i>
+                        <p>
+                            Pedidos
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                          <a href="{{route('pedidos.index')}}" class="nav-link">
                             <i class="far fa-circle nav-icon"></i>
-                            <p>Listar Usuarios</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('users.create') }}" class="nav-link">
+                            <p>Listar pedidos</p>
+                          </a>
+                        </li>
+                        <li class="nav-item">
+                          <a href="{{route('pedidos.create')}}" class="nav-link">
                             <i class="far fa-circle nav-icon"></i>
-                            <p>Agregar Usuarios</p>
-                        </a>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-edit"></i>
-                    <p>
-                        Medicamentos
-                        <i class="fas fa-angle-left right"></i>
-                    </p>
-                </a>
-                <ul class="nav nav-treeview">
-                    <li class="nav-item">
-                        <a href="{{ route('medicamento.index') }}" class="nav-link">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Listar Medicamentos</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('medicamento.create') }}" class="nav-link">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Agregar Medicamentos</p>
-                        </a>
-                    </li>
+                            <p>Crear pedidos</p>
+                          </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
 
 
-                </ul>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-edit"></i>
-                    <p>
-                        Pedidos
-
-                        <i class="fas fa-angle-left right"></i>
-
-                    </p>
-                </a>
-                <ul class="nav nav-treeview">
-                    <li class="nav-item">
-                      <a href="{{route('pedidos.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Listar pedidos</p>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="{{route('pedidos.create')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Crear pedidos</p>
-                      </a>
-                    </li>
-
-                  </ul>
-            </li>
-
-
-        </ul>
-    </nav>
-    <!-- /.sidebar-menu -->
 </div>
